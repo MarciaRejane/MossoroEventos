f80a84b8b2f42f26938b788355b8b06208938b27
Marcia Rejane <marciarejane230@gmail.com>
2026-03-09 11:21:49 -0300

    style: ajustado layout e estilos globais

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "../components/custom/Header/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
       <body
         className="antialiased">
+        <Header />
         {children}
       </body>
     </html>
