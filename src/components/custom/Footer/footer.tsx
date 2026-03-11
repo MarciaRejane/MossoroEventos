b4aac56f6521659b31498fc6f47b81d9df8b01f4
Marcia Rejane <marciarejane230@gmail.com>
2026-03-11 10:11:34 -0300

    feat: criado componente footer

---
+++ src/components/custom/Footer/footer.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { FOOTER_DATA } from "@/src/config/footer";
+import Link from "next/link";
+
+export default function Footer() {
+
+
+  return (
+    <footer className="bg-blue-main p-10  flex flex-col md:flex-row gap-10 justify-between">
+      <div>
+        <div>
+          <Image
+            src="/assets/Logo.png"
+            width={60}
+            height={60}
+            alt="Logo de mossoró eventos"
+            className="md:w-16 md:h-16 w-16 h-16"
+          />
+          <div className="flex flex-col text-white mb-2">
+            <h1 className="text-lg">Mossoró meu pais</h1>
+            <p>Cidade junina</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-white flex flex-col gap-6">
+        <p className="text-lg font-bold">Contato:</p>
+        {FOOTER_DATA.contato.map((item, index) => {
+          const Icon = item.icon
+          return (
+            <div key={index} className="flex flex-col ">
+              <Link href={"/"}><Icon />{item.value}</Link>
+            </div>
+          )
+        })}
+
+      </div>
+      <div className="flex flex-col">
+        <p className="text-lg text-white font-bold">Social:</p>
+        <div className="flex flex-row gap-6 text-white">
+          {FOOTER_DATA.social.map((item, index) => {
+            const Icon = item.icon
+            return (
+              <Link key={index} href={item.href} target="_blank" rel="noopener noreferrer"><Icon /></Link>
+            )
+          })}
+        </div>
+      </div>
+    </footer>
+  )
+}
