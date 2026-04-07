ac725229942b5a22921eae29c3280d26b25d6992
Marcia Rejane <marciarejane230@gmail.com>
2026-04-07 09:17:40 -0300

    feat: criado tela de história

---
+++ src/app/(public)/history/page.tsx
@@ -0,0 +1,9 @@
+import { HistoryContainer } from "@/src/features/history/components/Sections/historyContainer";
+
+export default function History() {
+  return (
+    <main>
+      <HistoryContainer />
+    </main>
+  )
+}
