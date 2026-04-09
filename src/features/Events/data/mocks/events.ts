d08e501f9d5aaa9982e41969a6c0f0b853afbf48
Marcia Rejane <marciarejane230@gmail.com>
2026-04-09 10:29:38 -0300

    chore: adicionado dados mockados de eventos

---
+++ src/features/Events/data/mocks/events.ts
@@ -0,0 +1,73 @@
+import type { Event } from "@/src/@types/events";
+
+export const event: Event[] = [
+  {
+    id: "1",
+    imageUrl: "/assets/chuva-de-bala.png",
+    category: "Show",
+    title: "Réveilon de Mossoró",
+    date: "31/12/2025",
+    time: "00h",
+    location: "Praça da Convivência",
+  },
+
+  {
+    id: "2",
+    imageUrl: "/assets/chuva-de-bala.png",
+    category: "Show",
+    title: "Encerramento Mossoró Cidade Junina",
+    date: "29/06/2026",
+    time: "21h",
+    location: "Praça Rodolfo Fernandes",
+  },
+
+  {
+    id: "3",
+    imageUrl: "/assets/chuva-de-bala.png",
+    category: "Teatro",
+    title: "Cantata de Natal",
+    date: "19/12/2026",
+    time: "20h",
+    location: "Teatro Municipal Dix-Huit Rosado",
+  },
+
+  {
+    id: "4",
+    imageUrl: "/assets/chuva-de-bala.png",
+    category: "Gastronomia",
+    title: "Festival Gastronômico",
+    date: "17/04/2026",
+    time: "18h",
+    location: "Praça da Convivência",
+  },
+
+  {
+    id: "5",
+    imageUrl: "/assets/chuva-de-bala.png",
+    category: "Cultura",
+    title: "Carnaval de Mossoró",
+    date: "13/02/2026",
+    time: "14h",
+    location: "Avenida Rio Branco",
+  },
+
+  {
+    id: "6",
+    imageUrl: "/assets/chuva-de-bala.png",
+    category: "Infantil",
+    title: "Arraiá Kids",
+    date: "16/06/2026",
+    time: "15h",
+    location: "Parque das Crianças",
+  },
+
+  {
+    id: "7",
+    imageUrl: "/assets/chuva-de-bala.png",
+    category: "Esporte",
+    title: "Corrida de Mossoró",
+    date: "11/04/2026",
+    time: "6h",
+    location: "Praça Rodolfo Fernandes",
+  },
+];
