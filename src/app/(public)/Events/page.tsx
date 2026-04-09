2493d25634e0708f7840d648c8c6992073c37c31
Marcia Rejane <marciarejane230@gmail.com>
2026-04-09 10:12:39 -0300

    criado tela de Event

---
+++ src/app/(public)/Events/page.tsx
@@ -0,0 +1,9 @@
+import { EventContainer } from "@/src/features/Events/components/Sections/EventContainer";
+
+export default function Event() {
+  return (
+    <main>
+      <EventContainer />
+    </main>
+  )
+}
