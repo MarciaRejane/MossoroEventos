4b4971270189a5e6e59a2f6ebe816606f035b990
Marcia Rejane <marciarejane230@gmail.com>
2026-04-13 09:19:15 -0300

    refactor: ajustado listagem para receber eventos filtrados

--- src/features/Events/components/EventList.tsx
+++ src/features/Events/components/EventList.tsx
@@ -1,13 +1,16 @@
 "use client"
 
+import { Event } from "@/src/@types/events";
 import { EventCard } from "@/src/components/custom/event/EventCard"
 import { Button } from "@/src/components/ui/button";
-import { event } from "@/src/features/Events/data/mocks/events";
 
-export function EventList() {
+type EventListProps = {
+  events: Event[];
+}
+export function EventList({ events }: EventListProps) {
   return (
     <div className="py-4 px-8 w-full md:max-w-2xs">
-      {event.map((item, index) => (
+      {events.map((item) => (
         <Button className="p-0 h-auto w-full block" key={item.id} variant={"ghost"}>
           <EventCard event={item} />
         </Button>
