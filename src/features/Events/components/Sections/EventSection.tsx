ba83aead3334b66055320651025b9b9ac83ccbf6
Marcia Rejane <marciarejane230@gmail.com>
2026-04-13 09:20:52 -0300

    refactor: integrado hook de filtro e search na seção de eventos

--- src/features/Events/components/Sections/EventSection.tsx
+++ src/features/Events/components/Sections/EventSection.tsx
@@ -1,16 +1,19 @@
+"use client"
 import { ScrollArea, ScrollBar } from "@/src/components/ui/scroll-area";
-import { SearchFilterConatiner } from "../SearchFilter/SearchFilterContainer";
+import { SearchFilterContainer } from "../SearchFilter/SearchFilterContainer";
 import { EventList } from "../EventList";
+import { useEvents } from "../../hook/useEvents";
 
 export function EventSction() {
+  const { filteredEvents, category, setCategory, search, setSearch } = useEvents();
   return (
-    <section className="h-screen">
+    <section className="min-h-screen">
       <ScrollArea className="h-full">
         <div className="sticky bg-white top-0 left-0 z-50 p-8 shadow-md">
-          <SearchFilterConatiner />
+          <SearchFilterContainer search={search} setSearch={setSearch} category={category} setCategory={setCategory} />
         </div>
 
-        <EventList />
+        <EventList events={filteredEvents} />
         <ScrollBar orientation="vertical" />
       </ScrollArea>
     </section>
