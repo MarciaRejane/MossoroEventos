d44bc21e11a37dc14884654672b45bc674d54023
Marcia Rejane <marciarejane230@gmail.com>
2026-04-13 09:12:47 -0300

    feat: adicionado função de normalização de texto para busca

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function normalize(text: string) {
+  return text
+    .toLowerCase()
+    .normalize("NFD") //Separa acento da letra
+    .replace(/[\u0300-\u036f]/g, ""); //remove acentos
 }
