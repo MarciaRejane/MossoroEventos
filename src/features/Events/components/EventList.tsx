b4bd749ec8b3d028cb6e10b67eab8de949a78703
Marcia Rejane <marciarejane230@gmail.com>
2026-04-24 10:54:20 -0300

    feat: implementado listagem de eventos com scroll e seleção para exibir no mapa

--- src/features/Events/components/EventList.tsx
+++ src/features/Events/components/EventList.tsx
@@ -2,19 +2,28 @@
 
 import { EventItem } from "@/src/@types/events";
 import { EventCard } from "@/src/components/custom/event/EventCard"
-import { Button } from "@/src/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/src/components/ui/scroll-area";
 
 type EventListProps = {
   events: EventItem[];
-}
-export function EventList({ events }: EventListProps) {
+  onSelect: (event: EventItem) => void;
+};
+
+export function EventList({ events, onSelect }: EventListProps) {
   return (
-    <div className="py-4 px-8 w-full md:max-w-2xs">
-      {events.map((item) => (
-        <Button className="p-0 h-auto w-full block" key={item.id} variant={"ghost"}>
-          <EventCard event={item} showImage />
-        </Button>
-      ))}
+    <div className="h-full bg-gray rounded-lg shadow-md shadow-gray-4 flex justify-center">
+      {events.length === 0 ? (
+        <p className="text-blue-main font-bold px-8 text-center">Nenhum evento encontrado</p>
+      ) : (
+        <ScrollArea className="h-[40rem] w-full px-2 py-4">
+          {events.map((item) => (
+            <div className="h-full w-full block cursor-pointer" key={item.id} onClick={() => onSelect(item)}>
+              <EventCard event={item} showImage />
+            </div>
+          ))}
+          <ScrollBar orientation="vertical" className="bg-gray-2 [&>div]:bg-brown-light [&>div]:rounded-full" />
+        </ScrollArea>
+      )}
     </div>
   )
 }
