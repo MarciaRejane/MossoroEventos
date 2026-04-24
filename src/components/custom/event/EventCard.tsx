d5db394063299298bf308fbce1cb1036a73b8bd9
Marcia Rejane <marciarejane230@gmail.com>
2026-04-24 11:07:47 -0300

    feat: adicionado variant 'map' no EventCard para uso no mapa

--- src/components/custom/event/EventCard.tsx
+++ src/components/custom/event/EventCard.tsx
@@ -7,24 +7,29 @@ import { EventInformation } from "./EventInformation";
 type eventItemProps = {
   event: EventItem;
   badgeVariant?: "default" | "category";
+  variant?: "default" | "map";
   showImage?: boolean;
 };
 
-export function EventCard({ event, showImage, badgeVariant = "default" }: eventItemProps) {
+export function EventCard({ event, showImage, badgeVariant = "default", variant = "default" }: eventItemProps) {
+  const isMap = variant === "map";
   return (
-    <Card className="flex flex-col border hover:shadow-gray-4 border-gray-2 pt-0.5 bg-white rounded-lg overflow-hidden mb-4">
+    <Card className=" flex flex-col border hover:shadow-gray-4 border-gray-2 pt-0.5 bg-white rounded-lg overflow-hidden mb-4">
       {showImage && event.imageUrl && (
-        <div className="h-50 w-full relative">
+        <div className="h-52 w-full relative">
           <Image
             fill
-            priority
+            sizes="(max-width: 768px) 100vw, 50vw"
+            loading="eager"
             className="object-cover rounded-t-lg"
             src={event.imageUrl}
             alt={event.title} />
         </div>
       )}
       <CardContent className="flex flex-col gap-1 pt-5">
-        <EventBadge category={event.category} variant={badgeVariant} />
+        {!isMap && (
+          <EventBadge category={event.category} variant={badgeVariant} />
+        )}
         <CardTitle className="text-blue-main font-bold text-base">{event.title}</CardTitle>
         <div className="text-gray-5 font-medium text-md">
           <EventInformation
@@ -34,7 +39,6 @@ export function EventCard({ event, showImage, badgeVariant = "default" }: eventI
           />
         </div>
       </CardContent>
-
     </Card>
   )
 }
