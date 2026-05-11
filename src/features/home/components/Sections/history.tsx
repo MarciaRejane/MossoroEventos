7932727a4da15bfc53347696b557f83415242dcd
Marcia Rejane <marciarejane230@gmail.com>
2026-05-11 14:34:20 -0300

    style: refinado layout da seção de história

--- src/features/home/components/Sections/history.tsx
+++ src/features/home/components/Sections/history.tsx
@@ -7,19 +7,20 @@ export function SectionHistory() {
     <section className="px-8 py-5">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-10 items-center">
         <div className="space-y-6">
-          <h2 className=" text-3xl md:text-4xl font-bold text-blue-main">Conheça Mossoró</h2>
-          <div className="text-lg font-light text-gray-5 space-y-4 ">
-            <p>Mossoró é das maiores cidades do Rio Grande do Norte e destaca-se no cenário nacional por sua rica história e cultura vibrante. A cidade é mundialmente conhecida pelo espetáculo "Chuva de Bala no País de Mossoró", que relembra a resistência da população ao ataque do bando de Lampião.</p>
-            <p>Durante o mês de junho, Mossoró se transforma no palco do maior São João do mundo, com uma programação que inclui shows nacionais, apresentações culturais, gastronomia típica e muito mais.</p>
+          <h2 className=" text-3xl md:text-4xl font-bold text-blue-5 mb-2">Conheça Mossoró</h2>
+          <div className="h-1 w-20 bg-[#8B4513] mb-6 rounded-full"></div>
+          <div className="text-lg font-light text-gray-5 space-y-4 leading-relaxed">
+            <p>Mossoró é das maiores cidades do Rio Grande do Norte e destaca-se no cenário nacional por sua rica história e cultura vibrante. A cidade é mundialmente conhecida pelo espetáculo <strong className="text-brown-5">"Chuva de Bala no País de Mossoró"</strong>, que relembra a resistência da população ao ataque do bando de Lampião.</p>
+            <p>Durante o mês de junho, Mossoró se transforma no palco do <strong className="text-brown-5">maior São João do mundo</strong>, com uma programação que inclui shows nacionais, apresentações culturais, gastronomia típica e muito mais.</p>
           </div>
           <Link href={"/history"}>
-            <Button variant={"secondary"} className="w-fit p-5 bg-brown-light hover:bg-brown-medium font-semibold text-md text-white cursor-pointer">
+            <Button variant={"secondary"} className="w-fit py-3 px-8 bg-brown-4 hover:bg-brown-5 font-semibold text-md text-white cursor-pointer rounded-xl transition-all shadow-md">
               Conheça Nossa História
             </Button>
           </Link>
         </div>
 
-        <div className="relative w-full h-64 md:w-[150rem]  md:h-[400px]">
+        <div className="relative rounded-2xl shadow-2xl shadow-gray-4 w-full md:w-[150rem]  h-[400px]">
           <Image
             src="/assets/conheca-historia.png"
             fill
