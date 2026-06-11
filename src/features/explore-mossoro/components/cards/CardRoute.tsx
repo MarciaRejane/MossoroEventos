fdf8787a23529eabe0d95bf322a445066bdd23fe
Marcia Rejane <marciarejane230@gmail.com>
2026-06-11 17:27:27 -0300

    feat: implementado card de navegação e de transporte

--- src/features/explore-mossoro/components/cards/CardRoute.tsx
+++ src/features/explore-mossoro/components/cards/CardRoute.tsx
@@ -1,30 +1,42 @@
+"use client"
 import { Card, CardContent, CardDescription, CardFooter, CardTitle } from "@/src/components/ui/card";
 import Link from 'next/link';
 import { BUTTON_ROUTES } from "@/src/features/explore-mossoro/mocks/buttonRoutes";
+import { getRouteUrl } from "@/src/lib/routes";
 
+type CardRouteProps = {
+  lat: number;
+  lng: number;
+  name?: string;
+};
 
-export function CardRoute() {
+export function CardRoute({ lat, lng, name }: CardRouteProps) {
   return (
     <Card className="shadow-xl px-8 w-full shadow-gray-3 border border-gray-3">
       <CardTitle className="text-blue-5">Rotas e Trasporte</CardTitle>
       <CardContent className='flex flex-col md:flex-row justify-center gap-4 px-0'>
+        {BUTTON_ROUTES.map((item) => {
+          const Icon = item.icon;
 
-        {
-          BUTTON_ROUTES.map((item) => {
-            const Icon = item.icon;
-            return (
-              <Link href={"/"} key={item.title} className={`flex items-center gap-2 w-full py-3 px-5 border rounded-md border-gray-5 hover:bg-blue-1`}>
-                <div className={`${item.iconColor} p-2 rounded-xl`}>
-                  <Icon />
-                </div>
+          const href = getRouteUrl(
+            item.type,
+            lat,
+            lng,
+            name
+          );
+          return (
+            <Link href={href} target="_blank" key={item.title} className={`flex items-center gap-2 w-full py-3 px-5 border rounded-md border-gray-5 hover:bg-blue-1`}>
+              <div className={`${item.iconColor} p-2 rounded-xl`}>
+                <Icon />
+              </div>
 
-                <div className='flex flex-col'>
-                  <span className='text-md font-bold'>{item.title}</span>
-                  <span className='text-sm'>{item.description}</span>
-                </div>
-              </Link>
-            )
-          })
+              <div className='flex flex-col'>
+                <span className='text-md font-bold'>{item.title}</span>
+                <span className='text-sm'>{item.description}</span>
+              </div>
+            </Link>
+          )
+        })
         }
       </CardContent>
       <CardFooter className='bg-blue-1 flex items-center flex-wrap gap-1 p-5 rounded-2xl'>
